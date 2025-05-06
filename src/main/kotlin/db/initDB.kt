4bb2ace9ac8b461dc5bcfa34f120b804024450f0
Amir Shahzad <amir.shahzad@evampsaanga.com>
2025-05-06 12:58:32 +0500

    Connected localhost database to Remote database RAILWAY MYSQL updated crediential

--- src/main/kotlin/db/initDB.kt
+++ src/main/kotlin/db/initDB.kt
@@ -3,15 +3,27 @@ import db.tables.UsersTable
 import io.ktor.server.application.Application
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.initDB() {
     val url = environment.config.property("database.url").getString()
-    val user = environment.config.property("database.user").getString()
-    val driver = environment.config.property("database.driver").getString()
-    val password = System.getenv("ktor_blog_database_password")
-    println("password $password")
+    val host = environment.config.property("database.host").getString() ?: "localhost"
+    val port = environment.config.property("database.port").getString() ?: 3306
+    val databaseName = environment.config.property("database.name").getString() ?: "railway"
+    val user = environment.config.property("database.user").getString() ?: "root"
+    val password = environment.config.property("database.password").getString() ?: ""
+    val driver = environment.config.property("database.driver").getString()?:""
 
+
+    println("host $host")
+    println("port $port")
+    println("databaseName $databaseName")
+    println("user $user")
+    println("password $password")
+    println("driver $driver")
+    println("url $url")
     val db = Database.connect(
         url = url,
         user = user,
@@ -20,6 +32,7 @@ fun Application.initDB() {
     )
 
     transaction(db) {
+        addLogger(StdOutSqlLogger)
         SchemaUtils.create(UsersTable, BlogsTable)
     }
 }
