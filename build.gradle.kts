edc71efc067e5e5dd52721cbdee69daf0a233a15
Amir Shahzad <amir.shahzad@evampsaanga.com>
2025-05-06 12:05:09 +0500

    jar path define in gridle.file

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,3 +40,25 @@ dependencies {
     implementation("mysql:mysql-connector-java:8.0.33")
     implementation("org.mindrot:jbcrypt:0.4")
 }
+
+tasks.getByName("build").finalizedBy("installDist")
+
+
+
+tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
+    manifest {
+        attributes["Main-Class"] =application.mainClass
+    }
+
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all the dependencies
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
